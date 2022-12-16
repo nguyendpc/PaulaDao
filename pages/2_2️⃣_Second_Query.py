aa7053af487b67be2f0c084e1adcc79c71421bb7
Nguyên Đinh <dpcnguyen5690@gmail.com>
2022-12-16 16:04:38 +0700

    cm

--- pages/2_2️⃣_Second_Query.py
+++ pages/2_2️⃣_Second_Query.py
@@ -30,20 +30,23 @@ def read_File(sheet="in",nrows=None):
     return result
 
 def trend_Violent(BOSTONCRIME,MONTH=None):
-    Violent  = [ 0 for i in range(0,6)]
-    Drug = [ 0 for i in range(0,6)]
+    Violent  = [ 0 for i in range(0,6)] 
+    Drug = [ 0 for i in range(0,6)] 
     Shooting = [ 0 for i in range(0,6)]
-    Trend = []
-    
+    Trend = [] # dùng cho ý sau, BOSTONCRIME là toàn bộ dữ liệu, Trend chỉ giữ lại dòng nào thuộc 2 type offense
     for b in BOSTONCRIME:
-        for v in Violent_Key:
-            if v.upper() in b["OFFENSE_DESCRIPTION"]:
-                m = int(b["MONTH"]) -1 
-                Violent[m] = Violent[m] + 1
-                Shooting[m] = Shooting[m] + b["SHOOTING"]
-                b["OFFENSE_CODE_GROUP"] = 1
+        # dò từng keyword của loại Violent để kiểm tra b có chứa keyword type Violent k
+        for v in Violent_Key: 
+            # v.upper() là biến chữ thành viết hoa, do mảng tạo ở trên là chữ thường, excel là hoa
+            if v.upper() in b["OFFENSE_DESCRIPTION"]: # kiểm tra xem giá trị của v.upper() có nằm trong b["OFFENSE_DESCRIPTION"] 
+                # ý tưởng tìm m là tương tự q1 offense_DISTRICT(BOSTONCRIME,DISTRICT) chỗ DISTRICT[b["DISTRICT"]]
+                m = int(b["MONTH"]) -1  # int(b["MONTH"]) để đảm bảo value của b["MONTH"] là 1 con số
+                Violent[m] = Violent[m] + 1 
+                Shooting[m] = Shooting[m] + b["SHOOTING"] # chỗ này b["SHOOTING"] chỉ nhận giá trị 0,1
+                b["OFFENSE_CODE_GROUP"] = 1 # excel k có OFFENSE_CODE_GROUP nên mình tự gán Violent là loại 1, dùng về sau
                 Trend.append(b)
-                break
+                break # break vì mỗi dòng b mình chỉ cần dò xem chỉ cần có 1 key của Violent nằm trong OFFENSE_DESCRIPTION là dừng
+        # dò từng keyword của loại Violent để kiểm tra b có chứa keyword type Drug k, tương tự khúc trên
         for d in Drug_Key:
             if d.upper() in b["OFFENSE_DESCRIPTION"]:
                 m = int(b["MONTH"]) -1 
@@ -58,7 +61,8 @@ def plot_LineChart(Violent,Drug):
     data_plot = pd.DataFrame({
             'Violent': Violent,
             'Drug': Drug
-            },index=list_Month)
+            })
+    st.write(data_plot)
     fig = plt.figure()
     sns.lineplot(data=data_plot,estimator=sum, ci=None,  color='lightblue')
     st.pyplot(fig)
@@ -95,17 +99,16 @@ def main():
         )
         if is_Month:
             # create dict BOSTONCRIME by month
-            p = pd.Period('2022-{}'.format(is_Month))
-            num = p.days_in_month
-            idx = pd.date_range(start='2022-12', freq='D', periods=num)
-            Day_Of_Month = np.array(idx.day_name())
-            Date_Of_Month = [i for i in range(1,num+1)]
+            p = pd.Period('2022-{}'.format(is_Month)) # khi chọn tháng = 1 nó sẽ tạo ra p='2022-1'
+            num = p.days_in_month # là số ngày của tháng đó = 31
+            idx = pd.date_range(start='2022-12', freq='D', periods=num) # tạo ra cái list 31 ngày từ '2022-12-01' ->'2022-12-31'
+            Day_Of_Month = np.array(idx.day_name()) # ngày của tháng dạng chữ tính từ ngày đầu tháng, k phải từ thứ 2
+            Date_Of_Month = [i for i in range(1,num+1)]  
             ToTal = [0 for i in range(0,num)]
             Shooting = [0 for i in range(0,num)]
-            
             for row in Date_Of_Month:
                 for b in Trend:
-                    p = pd.Period(b["OCCURRED_ON_DATE"], freq="H")
+                    p = pd.Period(b["OCCURRED_ON_DATE"], freq="H") # định dạng lại kiểu dữ liệu thời gian cho OCCURRED_ON_DATE
                     d = p.day
                     m = p.month
                     if m == is_Month and d == row:
